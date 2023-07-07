551b2a486079aa763aaf2ee8a5afc7e44a1638dd
HamdanSCode <hamdan.mobeen@gmail.com>
2023-07-07 08:29:56 -0700

    profile endpoint update

--- data.sql
+++ data.sql
@@ -13,23 +13,25 @@ CREATE TABLE users
   first_name varchar(20) NOT NULL,
   last_name varchar(20) NOT NULL,
   location varchar(64) NOT NULL,
+  profile_image varchar(24) NOT NULL,
+  about_me varchar(512) NOT NULL,
   created_at timestamp DEFAULT (CURRENT_TIMESTAMP)
  );
 
 INSERT INTO users 
-(email, birth_date, first_name, last_name, location) 
+(email, birth_date, first_name, last_name, location, profile_image, about_me) 
 VALUES 
-('nic_wilson@live.com', '1990-12-18', 'nic', 'wilson', 'Hell, MI');
+('nic_wilson@live.com', '1990-12-18', 'nic', 'wilson', 'Hell, MI',"upload/pp_default.jpg","ABOUT_ME_DEFAULT_VALUE");
 
 INSERT INTO users 
-(email, birth_date, first_name, last_name, location) 
+(email, birth_date, first_name, last_name, location, profile_image, about_me) 
 VALUES 
-('hamdan.mobeen@gmail.com', '1997-07-09', 'hamdan', 'syed', 'Pooville, CA');
+('hamdan.mobeen@gmail.com', '1997-07-09', 'hamdan', 'syed', 'Pooville, CA',"upload/pp_default.jpg","ABOUT_ME_DEFAULT_VALUE");
 
 INSERT INTO users 
-(email, birth_date, first_name, last_name, location) 
+(email, birth_date, first_name, last_name, location, profile_image, about_me) 
 VALUES 
-('axaxotl@gmail.com', '1996-07-22', 'Brandon', 'Ross', "Michael's Dungeon");
+('axaxotl@gmail.com', '1996-07-22', 'Brandon', 'Ross', "Michael's Dungeon","upload/pp_default.jpg","ABOUT_ME_DEFAULT_VALUE");
 
 
 -- ------------------- CODES
